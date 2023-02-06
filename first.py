b8763a033c60f05c7e4251ef1c136a5d39da3f3d
Alhassan Jerijees <jerijeesalhassan@gmail.com>
2023-02-06 13:29:16 +0100

    my third commit

--- first.py
+++ first.py
@@ -6,4 +6,5 @@ def power(base, exp):
     if exp == 0: return 1
     else: return base * power(base, exp - 1)
 
-def 
+def display():
+    print('hello  git')
---
+++ second.py
@@ -0,0 +1,3 @@
+def power(base, exp):
+    if exp == 0: return 1
+    else: return base * power(base, exp - 1)
