e125d851e3830c7327b4155b369f4a45293da6b8
Alhassan Jerijees <jerijeesalhassan@gmail.com>
2023-02-06 13:47:44 +0100

    fixed the bug

--- second.py
+++ second.py
@@ -1,3 +1,7 @@
 def power(base, exp):
     if exp == 0: return 1
     else: return base * power(base, exp - 1)
+
+def power(base, exp):
+    if exp == 0: return 1
+    else: return base * power(base, exp - 1)
