23dae8028c4ad4f5f58d275637fcc3762e3f58cd
Alhassan Jerijees <jerijeesalhassan@gmail.com>
2023-02-06 13:59:09 +0100

    Fix: Argument

--- second.py
+++ second.py
@@ -2,6 +2,6 @@ def power(base, exp):
     if exp == 0: return 1
     else: return base * power(base, exp - 1)
 
-def power(base, exp):
+def power(b, exp):
     if exp == 0: return 1
-    else: return base * power(base, exp - 1)
+    else: return base * power(b, exp - 1)
